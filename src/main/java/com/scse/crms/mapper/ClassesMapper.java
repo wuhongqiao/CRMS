0d5628967e7017b4c9b1223a9426ad4799b82a32
Hometown <809033066@qq.com>
2018-03-01 21:34:54 +0800

    Signed-off-by: Hometown <809033066@qq.com>

--- src/main/java/com/scse/crms/mapper/ClassesMapper.java
+++ src/main/java/com/scse/crms/mapper/ClassesMapper.java
@@ -2,10 +2,11 @@ package com.scse.crms.mapper;
 
 import java.util.List;
 
+import com.scse.crms.vo.ClassesVo;
 import com.scse.crms.vo.SeatTable;
 
 public interface ClassesMapper {
-	public List selectSchedule();
-	public List selectSeat();
+	public List<ClassesVo> selectSchedule();
+	public List<SeatTable> selectSeat();
 	public int updateSeat(SeatTable seatTable);
 }
