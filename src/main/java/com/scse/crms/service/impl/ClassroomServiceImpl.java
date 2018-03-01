f9e3e26e5b7981aabeedbdb201d807b7177739d5
Hometown <809033066@qq.com>
2018-03-01 21:12:22 +0800

    Signed-off-by: Hometown <809033066@qq.com>

--- src/main/java/com/scse/crms/service/impl/ClassroomServiceImpl.java
+++ src/main/java/com/scse/crms/service/impl/ClassroomServiceImpl.java
@@ -1,11 +1,13 @@
 package com.scse.crms.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.scse.crms.mapper.ClassroomMapper;
 import com.scse.crms.po.Classroom;
 import com.scse.crms.service.ClassroomService;
 
+@Service
 public class ClassroomServiceImpl implements ClassroomService {
 	@Autowired
 	private ClassroomMapper classroomMapper; 
