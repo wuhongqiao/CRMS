ec224409c243c3ed58307ebae94be7d0a9832a44
Hometown <809033066@qq.com>
2018-03-07 17:15:45 +0800

    Signed-off-by: Hometown <809033066@qq.com>
    座位表页面对接

--- src/main/java/com/scse/crms/controller/ClassroomController.java
+++ src/main/java/com/scse/crms/controller/ClassroomController.java
@@ -6,21 +6,23 @@ import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.scse.crms.service.ClassroomService;
 
+@Controller
 public class ClassroomController {
 	@Autowired
 	ClassroomService classroomService;
 	
-	@RequestMapping(value="class.do", produces="text/html;charset=UTF-8")
+	@RequestMapping(value="/class.do", produces="text/html;charset=UTF-8")
 	@ResponseBody
-	public String selectSchedule(String crid) throws JsonGenerationException, JsonMappingException, IOException {
+	public String selectSchedule(String classid) throws JsonGenerationException, JsonMappingException, IOException {
 		//service.selectSchedule();
 		
-		return new ObjectMapper().writeValueAsString(classroomService.selectClassroom(crid));
+		return new ObjectMapper().writeValueAsString(classroomService.selectClassroom(classid));
 	}
 
 }
